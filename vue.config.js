188bb35d688fbc794703dee0e94360318f481def
yagy <logi_st@outlook.jp>
2021-10-04 21:41:02 +0900

    remove unnecessary files

--- vue.config.js
+++ vue.config.js
@@ -4,16 +4,6 @@ module.exports = {
       template: 'public/browser-extension.html',
       entry: './src/popup/main.js',
       title: 'Popup'
-    },
-    options: {
-      template: 'public/browser-extension.html',
-      entry: './src/options/main.js',
-      title: 'Options'
-    },
-    override: {
-      template: 'public/browser-extension.html',
-      entry: './src/override/main.js',
-      title: 'Override'
     }
   },
 
